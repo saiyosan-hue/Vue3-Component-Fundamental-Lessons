3f9f658e9506dfb4ac482c07f5e08f47ecf48dc3
saiyosan-hue <saiyosan@gmail.com>
2025-12-14 20:37:23 +0300

    Update app.js

--- slots/app.js
+++ slots/app.js
@@ -1,3 +1,13 @@
-const app = Vue.createApp({})
+const app = Vue.createApp({
 
+
+})
+.component('todo-item', {
+    template: '#todo-item-template',
+    data(){
+        return {
+            done: false
+        }
+    }
+})
 .mount('#app')
